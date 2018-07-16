1c30fab92dfc18dd55a95328c3dc6046cf467a0e
MichaelBone <Michael.Bone@senet.com.au>
2018-07-16 19:05:28 +0930

    Initial implementation.

--- scraper.js
+++ scraper.js
@@ -1,59 +1,240 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+// Parses the development application at the South Australian Adelaide Hills Council web site and
+// places them in a database.
+//
+// Michael Bone
+// 16th July 2018
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+let cheerio = require("cheerio");
+let request = require("request-promise-native");
+let sqlite3 = require("sqlite3").verbose();
+let pdf2json = require("pdf2json");
+let urlparser = require("url");
+let moment = require("moment");
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
+const DevelopmentApplicationsUrl = "https://www.ahc.sa.gov.au/Resident/planning-and-building/the-development-process/development-applications/development-applications-register";
+const CommentUrl = "mailto:mail@ahc.sa.gov.au";
+
+// Sets up an sqlite database.
+
+async function initializeDatabase() {
+    return new Promise((resolve, reject) => {
+        let database = new sqlite3.Database("data.sqlite");
+        database.serialize(() => {
+            database.run("create table if not exists [data] ([council_reference] text primary key, [address] text, [description] text, [info_url] text, [comment_url] text, [date_scraped] text, [date_received] text, [on_notice_from] text, [on_notice_to] text)");
+            resolve(database);
+        });
+    });
+}
+
+// Inserts a row in the database if it does not already exist.
+
+async function insertRow(database, developmentApplication) {
+    return new Promise((resolve, reject) => {
+        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        sqlStatement.run([
+            developmentApplication.applicationNumber,
+            developmentApplication.address,
+            developmentApplication.reason,
+            developmentApplication.informationUrl,
+            developmentApplication.commentUrl,
+            developmentApplication.scrapeDate,
+            developmentApplication.receivedDate,
+            null,
+            null
+        ], function(error, row) {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                if (this.changes > 0)
+                    console.log(`    Inserted new application \"${developmentApplication.applicationNumber}\" into the database.`);
+                sqlStatement.finalize();  // releases any locks
+                resolve(row);
+            }
+        });
+    });
+}
+
+// Parses the development applications.
+
+async function main() {
+    // Ensure that the database exists.
+
+    let database = await initializeDatabase();
+
+    // Retrieve the page contain the link to the PDF.
+
+	console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
+    let body = await request(DevelopmentApplicationsUrl);
+    let $ = cheerio.load(body);
+
+	let relativePdfUrl = null;
+	$("a[href$='.pdf']").each((index, element) => {
+		if ($(element).text() === "Development Applications Register")
+		    relativePdfUrl = element.attribs.href;
 	});
-}
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
+	if (relativePdfUrl === null) {
+		console.log("Could not find a link to the PDF that contains the development applications.");
+		return;
+	}
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
+	let pdfUrl = new urlparser.URL(relativePdfUrl, DevelopmentApplicationsUrl)
+	console.log(`Retrieving document: ${pdfUrl.href}`);
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
+	// Parse the PDF into a collection of PDF rows.  Each PDF row is simply an array of strings,
+	// being the text that has been parsed from the PDF.
+
+	let pdfParser = new pdf2json();
+	let pdfPipe = request({ url: pdfUrl.href, encoding: null }).pipe(pdfParser);
+	pdfPipe.on("pdfParser_dataError", error => console.error(error))
+	pdfPipe.on("pdfParser_dataReady", pdf => {
+		// Convert the JSON representation of the PDF into a collection of PDF rows.
+
+		console.log(`Parsing document.`);
+		let pdfRows = convertPdfToText(pdf);
+
+		for (let pdfRow of pdfRows) {
+			// console.log(pdfRow);
 		}
-
-		callback(body);
 	});
 }
 
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
+// Convert a parsed PDF into an array of rows.  This function is based on pdf2table by Sam Decrock.
+// See https://github.com/SamDecrock/pdf2table/blob/master/lib/pdf2table.js.
+//
+// Copyright (c) 2015 Sam Decrock <sam.decrock@gmail.com>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
+function convertPdfToText(pdf) {
+    let xComparer = (a, b) => (a.x > b.x) ? 1 : ((a.x < b.x) ? -1 : 0);
+    let yComparer = (a, b) => (a.y > b.y) ? 1 : ((a.y < b.y) ? -1 : 0);
 
-		readRows(db);
+    // Find the smallest Y co-ordinate for two texts with equal X co-ordinates.
 
-		db.close();
-	});
+    let smallestYValueForPage = [];
+
+    for (let pageIndex = 0; pageIndex < pdf.formImage.Pages.length; pageIndex++) {
+        let page = pdf.formImage.Pages[pageIndex];
+        let smallestYValue = null;  // per page
+        let textsWithSameXValues = {};
+
+        for (let textIndex = 0; textIndex < page.Texts.length; textIndex++) {
+            let text = page.Texts[textIndex];
+            if (!textsWithSameXValues[text.x])
+                textsWithSameXValues[text.x] = [];
+            textsWithSameXValues[text.x].push(text);
+        }
+
+        // Find smallest Y distance.
+
+        for (let x in textsWithSameXValues) {
+            let texts = textsWithSameXValues[x];
+            for (let i = 0; i < texts.length; i++) {
+                for (let j = 0; j < texts.length; j++) {
+                    if (texts[i] !== texts[j]) {
+                        let distance = Math.abs(texts[j].y - texts[i].y);
+                        if (smallestYValue === null || distance < smallestYValue)
+                            smallestYValue = distance;
+                    }
+                };
+            };
+        }
+
+        if (smallestYValue === null)
+            smallestYValue = 0;
+        smallestYValueForPage.push(smallestYValue);
+    }
+
+    // Find texts with similar Y values (in the range of Y - smallestYValue to Y + smallestYValue).
+
+    let myPages = [];
+
+    for (let pageIndex = 0; pageIndex < pdf.formImage.Pages.length; pageIndex++) {
+        let page = pdf.formImage.Pages[pageIndex];
+
+        let rows = [];  // store texts and their X positions in rows
+
+        for (let textIndex = 0; textIndex < page.Texts.length; textIndex++) {
+            let text = page.Texts[textIndex];
+
+            let foundRow = false;
+            for (let rowIndex = rows.length - 1; rowIndex >= 0; rowIndex--) {
+                // Y value of text falls within the Y value range, add text to row.
+
+                let maximumYdifference = smallestYValueForPage[pageIndex];
+                if (rows[rowIndex].y - maximumYdifference < text.y && text.y < rows[rowIndex].y + maximumYdifference) {
+                    // Only add value of T to data (which is the actual text).
+
+                    for (let index = 0; index < text.R.length; index++)
+                        rows[rowIndex].data.push({ text: decodeURIComponent(text.R[index].T), x: text.x });
+                    foundRow = true;
+                }
+            };
+
+            // Create a new row and add the text to the row.
+
+            if (!foundRow) {
+                let row = { y: text.y, data: [] };
+                for (let index = 0; index < text.R.length; index++)
+                    row.data.push({ text: decodeURIComponent(text.R[index].T), x: text.x });
+                rows.push(row);
+            }
+        };
+
+        // Sort each extracted row horizontally by X co-ordinate.
+
+        for (let index = 0; index < rows.length; index++)
+            rows[index].data.sort(xComparer);
+
+        // Sort rows vertically by Y co-ordinate.
+
+        rows.sort(yComparer);
+
+        // Add rows to pages.
+
+        myPages.push(rows);
+    };
+
+    // Flatten pages into rows.
+
+    let rows = [];
+
+    for (let pageIndex = 0; pageIndex < myPages.length; pageIndex++) {
+        for (let rowIndex = 0; rowIndex < myPages[pageIndex].length; rowIndex++) {
+            // Now that each row is made of objects extract the text property from the object.
+
+            let rowEntries = []
+            let row = myPages[pageIndex][rowIndex].data;
+            for (let index = 0; index < row.length; index++)
+                rowEntries.push(row[index].text);
+
+            // Append the extracted and ordered text into the return rows.
+
+            rows.push(rowEntries);
+        };
+    };
+
+    return rows;
 }
 
-initDatabase(run);
+main().catch(error => console.error(error));
