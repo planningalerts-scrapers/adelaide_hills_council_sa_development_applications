0e8a29491649079d4c3cc85550d5eaa2541ca38f
MichaelBone <Michael.Bone@senet.com.au>
2019-02-24 14:20:07 +1030

    Updated the development application register URL.

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ let pdf2json = require("pdf2json");
 let urlparser = require("url");
 let moment = require("moment");
 
-const DevelopmentApplicationsUrl = "https://www.ahc.sa.gov.au/Resident/planning-and-building/development-applications/development-applications-register";
+const DevelopmentApplicationsUrl = "https://www.ahc.sa.gov.au/ahc-resident/Pages/Planning-and-Building/Current-Development-Applications.aspx";
 const CommentUrl = "mailto:mail@ahc.sa.gov.au";
 
 // Sets up an sqlite database.
